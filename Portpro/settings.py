78c4a80f37ac412e4b399fa16375618562145c67
harshitraj33 <harshitpriv.3@gmail.com>
2026-02-25 15:40:52 +0530

    update

--- Portpro/settings.py
+++ Portpro/settings.py
@@ -105,13 +105,10 @@ USE_TZ = True
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
 STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = BASE_DIR / 'media'
