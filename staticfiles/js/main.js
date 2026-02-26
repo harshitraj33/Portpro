2da0fbae172c7fc0c1ce1975f237763d8cbc96c9
harshitraj33 <harshitpriv.3@gmail.com>
2026-02-26 16:24:23 +0530

    update

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -91,3 +91,20 @@ function stopGreeting() {
 
 window.startGreeting = startGreeting;
 window.stopGreeting = stopGreeting;
+
+import { SpeedInsights } from '@vercel/speed-insights/next';
+export default function RootLayout({
+  children,
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
